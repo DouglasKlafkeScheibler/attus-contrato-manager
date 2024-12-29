9b917a525a9090f2c879f70d7f3c4d31a9cd71eb
douglasklafkescheibler <douglasks99@gmail.com>
2024-12-29 17:14:59 -0300

    Ajuste URL heroku doc

--- src/main/java/br/com/attus/contratomanager/config/SwaggerConfig.java
+++ src/main/java/br/com/attus/contratomanager/config/SwaggerConfig.java
@@ -3,27 +3,31 @@ package br.com.attus.contratomanager.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @OpenAPIDefinition(
-    info = @Info(
-        title = "Gerenciamento de Contratos Jurídicos",
-        version = "1.0.0",
-        description = "Documentação da API de Gerenciamento de Contratos Jurídicos"
-    ),
+        info = @Info(
+                title = "Gerenciamento de Contratos Jurídicos",
+                version = "1.0.0",
+                description = "Documentação da API de Gerenciamento de Contratos Jurídicos"
+        ),
         servers = {
-            @Server(
-                    description = "Ambiente Heroku",
-                    url = "http://Teste:8080"
-            ),
-            @Server(
-                    description = "Ambiente Local",
-                    url = "http://localhost:8080"
-            )
+                @Server(
+                        description = "Ambiente Heroku",
+                        url = "https://attus-contrato-manager-f4b71e29a75f.herokuapp.com"
+                ),
+                @Server(
+                        description = "Teste",
+                        url = "http://localhost:8080"
+                )
         }
 )
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 }
